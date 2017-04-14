928ba33b6a9d5ae4b9a8c2074462374d5316aa6a
JSlovak <jasmine.slovak@gmail.com>
2017-04-13 14:47:09 -1000

    Initial Commit

---
+++ client.js
@@ -0,0 +1,9 @@
+/*jshint esversion: 6 */
+
+const net = require('net');
+
+const client = net.connect({port:8080}, ()=>{
+
+
+
+});
---
+++ server.js
@@ -0,0 +1,17 @@
+/*jshint esversion: 6 */
+
+const net = require('net');
+
+const server = net.createServer((socket)=>{
+
+
+
+
+
+
+});
+
+// This initiates the Server
+server.listen(port,()=> {
+
+});
